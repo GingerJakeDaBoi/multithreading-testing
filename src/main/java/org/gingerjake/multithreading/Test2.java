92fc599c164f8a1843fa081c93e5519014b40369
GingerJakeDaBoi <GingerJakeDaBoi@users.noreply.github.com>
2022-06-23 14:31:04 -0400

    fix formatting

--- src/main/java/org/gingerjake/multithreading/Test2.java
+++ src/main/java/org/gingerjake/multithreading/Test2.java
@@ -1,36 +1,36 @@
 package org.gingerjake.multithreading;
 
 class Test2 implements Runnable {
-   private Thread thread;
-   private final String threadName;
-   private final int threadDelay;
+    private Thread thread;
+    private final String threadName;
+    private final int threadDelay;
 
-   Test2(String name, int delay) {
-      threadName = name;
-      threadDelay = delay;
-      System.out.println("Creating " + threadName );
-   }
-   
-   public void run() {
-      System.out.println("Running " +  threadName );
-      System.out.println("Hello Planet");
-      for(int thread = 4; thread > 0; thread--) {
-         System.out.println("Thread: " + threadName + ", " + thread);
-         //let the thread sleep for a specified time (threadName)
-         try {
-            Thread.sleep(threadDelay);
-         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-         }
-      }
-      System.out.println("Thread " +  threadName + " exiting.");
-   }
-   
-   public void start () {
-      System.out.println("Starting " +  threadName );
-      if (thread == null) {
-         thread = new Thread (this, threadName);
-         thread.start ();
-      }
-   }
+    Test2(String name, int delay) {
+        threadName = name;
+        threadDelay = delay;
+        System.out.println("Creating " + threadName);
+    }
+
+    public void run() {
+        System.out.println("Running " + threadName);
+        System.out.println("Hello Planet");
+        for (int thread = 4; thread > 0; thread--) {
+            System.out.println("Thread: " + threadName + ", " + thread);
+            //let the thread sleep for a specified time (threadName)
+            try {
+                Thread.sleep(threadDelay);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Thread " + threadName + " exiting.");
+    }
+
+    public void start() {
+        System.out.println("Starting " + threadName);
+        if (thread == null) {
+            thread = new Thread(this, threadName);
+            thread.start();
+        }
+    }
 }
